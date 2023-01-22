8dd81369e8ee76662a6dd0fcd7acfb76c8bcd70a
BulZok <bigle1010@gmail.com>
2023-01-22 22:34:49 +0900

    Initial commit

---
+++ src/main/java/com/fastcampus/ch4/test.java
@@ -0,0 +1,9 @@
+package com.fastcampus.ch4;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println('\''+"개미");
+        System.out.println("\'"+"개미");
+
+    }
+}
